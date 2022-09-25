31f021ca64c02e4ffbb01e8b6d7878dfd1ba85d0
Mark Rumiantsev <markwwwork@yandex.ru>
2022-09-25 23:12:33 +0300

    Use gTTS method fot convert txt file to mp3. Add a input fieds for user.

--- main.py
+++ main.py
@@ -8,25 +8,29 @@ def pdf_to_mp3(file_path='Check.pdf', language='en'):
     if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':
         #return 'File exist'
 
+        print(f'[+] Original file: {Path(file_path).name}')
+        print('[+] Processing...')
+
         with pdfplumber.PDF(open(file=file_path, mode='rb')) as pdf:
             pages = [page.extract_text() for page in pdf.pages]
             
         text = ''.join(pages)
-
-        with open('text.txt', 'w') as file:
-            file.write(text)
-
         text = text.replace('\n', '')
 
-        with open('text1.txt', 'w') as file:
-            file.write(text)
-            
+        my_audio = gTTS(text=text, lang=language, slow=False)
+        file_name = Path(file_path).stem
+        my_audio.save(f'{file_name}.mp3')
+
+        return f'[+] {file_name}.mp3 successfully saved!\n---Have a niceday!---'
+
     else:
         return 'File not exist, please check the file path'
 
 
 def main():
-    print(pdf_to_mp3(file_path='pdf_files\Check.pdf'))
+    file_path = input("\nEnter a file path: ")
+    language = input("Choose language, for example 'en' or 'pl': ")
+    print(pdf_to_mp3(file_path=file_path, language=language))
 
 
 if __name__ == '__main__':
