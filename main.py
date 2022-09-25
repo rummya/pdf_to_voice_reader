d4178266fafefbbd652868733009913cff461f5b
Mark Rumiantsev <markwwwork@yandex.ru>
2022-09-25 16:04:34 +0300

    Add file reader, open file used mehod rb. Join all text adn extract line breaks

--- main.py
+++ main.py
@@ -3,18 +3,33 @@ import pdfplumber
 from pathlib import Path
 
 
-def pdf_to_mp3(file_path='test.pdf', language='en'):
+def pdf_to_mp3(file_path='Check.pdf', language='en'):
 
-    if Path(file_path).is_file and Path(file_path).suffix == '.pdf':
-        return 'File exist'
+    if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':
+        #return 'File exist'
+
+        with pdfplumber.PDF(open(file=file_path, mode='rb')) as pdf:
+            pages = []
+            for page in pdf.pages:
+                pages.append(page.extract_text())
+            
+        text = ''.join(pages)
+
+        with open('text.txt', 'w') as file:
+            file.write(text)
+
+        text = text.replace('\n', '')
+
+        with open('text1.txt', 'w') as file:
+            file.write(text)
+            
     else:
         return 'File not exist, please check the file path'
 
 
 def main():
-    print(pdf_to_mp3())
+    print(pdf_to_mp3(file_path='pdf_files\Check.pdf'))
 
 
 if __name__ == '__main__':
     main()
-
